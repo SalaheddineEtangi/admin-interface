204e31c70d941bea7ee09edb173ace13f07622cd
salah-eddine.etangi <salah-eddine.etangi@cdbdx.biz>
2021-05-21 11:43:50 +0200

    admin-interface

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -4,13 +4,24 @@ import {
     ListGroupItem,
     Button
 } from 'reactstrap';
+import {API_URL} from '../config';
+
+const deleteUser = id => {
+    fetch(`${API_URL}/users/delete/${id}`, { method: 'DELETE' })
+    window.location.reload()
+}
 
 const UsersListItem = props => {
     return(
         <ListGroup className="mt-4">
             <ListGroupItem>
                 <strong>{props.users.email}</strong>
-                <Button color="danger" className="float-end">Supprimer</Button>
+                <Button 
+                    color="danger" 
+                    className="float-end"
+                    onClick={() => deleteUser(props.users.id)}>
+                    Supprimer
+                </Button>
             </ListGroupItem>
         </ListGroup>
     )
